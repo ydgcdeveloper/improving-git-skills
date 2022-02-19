fcc0daba41391c6322691c291ed86f1f99802247
Yan David <ygomezcoba@gmail.com>
2022-02-19 14:29:22 -0500

    Add divide function

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ function sum(a, b) {
   return a + b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
 function difference(a, b) {
   return Math.abs(a - b);
 }
 
-console.log(sum(5, 10));
+function divide(a, b) {
+  return b == 0 ? 0 : a / b;
+}
+
+console.log(divide(5, 10));
 console.log(difference(5, 10));
